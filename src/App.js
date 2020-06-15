b02ffe20dc136062fcb2471cadfc46ec4a786669
Rodrigo Castro <rodrigocastro@iMac.lan>
2020-06-15 21:39:02 +0200

    61. sass

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-
+import HomePage from './homepage.component';
 function App() {
-  return <div>Hello World</div>
+  return <div><HomePage /></div>
 }
 
 export default App;
